6851c73b9ff79ccc913ebcf1e25eb3b777e4a94c
Samuel Adu <theadusamuel@gmail.com>
2024-08-08 19:51:06 +0100

    add code to toggle aria-expanded

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const accordions = document.querySelectorAll('.accordion');
+const details = document.querySelectorAll('.details');
 
 accordions.forEach((accordion) => {
-  accordion.addEventListener('click', function () {
+  accordion.addEventListener('click', function (event) {
+    const element = event.target;
+    const isExpanded = element.getAttribute('aria-expanded') === 'true';
+    element.setAttribute('aria-expanded', !isExpanded);
     accordion.classList.toggle('open');
   });
 });
